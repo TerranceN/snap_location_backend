bc92b19278e5dcf14d66a922f5fdc8e81790b8c8
Terrance Niechciol <TNiechciol@gmail.com>
2014-09-20 17:50:29 +0400

    Users can now have case sensitive unique ids, they are compared lowercase though

--- snap_location/snap_location_app/models.py
+++ snap_location/snap_location_app/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 class User(models.Model):
     display_name = models.CharField(max_length=20)
+    unique_name_display = models.CharField(max_length=10)
     unique_name = models.CharField(max_length=10, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.unique_name_display = self.unique_name
+            self.unique_name = self.unique_name.lower()
+        super(User, self).save(*args, **kwargs)
 
 class Relationship(models.Model):
     type = models.IntegerField(default=0)
--- snap_location/snap_location_app/views.py
+++ snap_location/snap_location_app/views.py
@@ -25,21 +25,24 @@ def add_relationship(request):
     try:
         first_user_name = request.GET['first_user']
         second_user_name = request.GET['second_user']
-        first_user = User.objects.get(unique_name=first_user_name)
-        second_user = User.objects.get(unique_name=second_user_name)
+        first_user = User.objects.get(unique_name=first_user_name.lower())
+        second_user = User.objects.get(unique_name=second_user_name.lower())
         min_id = min(first_user.id, second_user.id)
         max_id = max(first_user.id, second_user.id)
         Relationship.objects.create(first_user=min_id, second_user=max_id)
         return HttpResponse(json.dumps({'result': 'success'}))
     except MultiValueDictKeyError as e:
-        return HttpResponse(json.dumps({'result': 'missing arguments', 'long_error': e.message}))
+        return HttpResponse(json.dumps({'result': 'missing arguments', 'add_info': e.message}))
     except IntegrityError as e:
+        # this means the relationship already exists, so might as well return success
         return HttpResponse(json.dumps({'result': 'success'}))
+    except User.DoesNotExist as e:
+        return HttpResponse(json.dumps({'result': 'unknown user', 'add_info': e.message}))
 
 def show_friends(request):
     try:
         unique_name = request.GET['unique_name']
-        user = User.objects.get(unique_name=unique_name)
+        user = User.objects.get(unique_name=unique_name.lower())
         relationships = Relationship.objects.filter(Q(first_user=user.id) | Q(second_user=user.id))
         def get_relation_data(relation):
             other_user = None
@@ -47,7 +50,9 @@ def show_friends(request):
                 other_user = User.objects.get(id=relation.second_user)
             else:
                 other_user = User.objects.get(id=relation.first_user)
-            return {'display_name': other_user.display_name}
+            return {'display_name': other_user.display_name, 'unique_name': other_user.unique_name_display}
         return HttpResponse(json.dumps({'result': 'success', 'data': map(get_relation_data, relationships)}))
     except MultiValueDictKeyError as e:
         return HttpResponse(json.dumps({'result': 'missing arguments', 'long_error': e.message}))
+    except User.DoesNotExist as e:
+        return HttpResponse(json.dumps({'result': 'unknown user', 'add_info': e.message}))
