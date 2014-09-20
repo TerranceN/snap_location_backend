a34cd8c2d7e5829340c4d858fa12d81244bc34b2
Terrance Niechciol <TNiechciol@gmail.com>
2014-09-20 10:44:53 +0400

    Fixed show_friends function, fixed bug in add_relationship that added the first user twice

--- snap_location/snap_location_app/views.py
+++ snap_location/snap_location_app/views.py
@@ -27,16 +27,27 @@ def add_relationship(request):
         second_user_name = request.GET['second_user']
         first_user = User.objects.get(unique_name=first_user_name)
         second_user = User.objects.get(unique_name=second_user_name)
-        Relationship.objects.create(first_user=first_user.id, second_user=second_user.id)
+        min_id = min(first_user.id, second_user.id)
+        max_id = max(first_user.id, second_user.id)
+        Relationship.objects.create(first_user=min_id, second_user=max_id)
         return HttpResponse(json.dumps({'result': 'success'}))
     except MultiValueDictKeyError as e:
         return HttpResponse(json.dumps({'result': 'missing arguments', 'long_error': e.message}))
+    except IntegrityError as e:
+        return HttpResponse(json.dumps({'result': 'success'}))
 
 def show_friends(request):
     try:
         unique_name = request.GET['unique_name']
         user = User.objects.get(unique_name=unique_name)
         relationships = Relationship.objects.filter(Q(first_user=user.id) | Q(second_user=user.id))
-        return HttpResponse(json.dumps({'result': 'success', 'data': map(lambda x: {'display_name': User.objects.get(id=x.first_user).display_name}, relationships)}))
+        def get_relation_data(relation):
+            other_user = None
+            if (relation.first_user == user.id):
+                other_user = User.objects.get(id=relation.second_user)
+            else:
+                other_user = User.objects.get(id=relation.first_user)
+            return {'display_name': other_user.display_name}
+        return HttpResponse(json.dumps({'result': 'success', 'data': map(get_relation_data, relationships)}))
     except MultiValueDictKeyError as e:
         return HttpResponse(json.dumps({'result': 'missing arguments', 'long_error': e.message}))
